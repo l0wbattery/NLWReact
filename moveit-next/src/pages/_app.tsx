194ad5feac179c98a2d77a5e3d0f6a53f62eb0ea
Victor Scherer <victorescherer@hotmail.com>
2021-02-25 13:57:28 -0300

    Third class

--- moveit-next/src/pages/_app.tsx
+++ moveit-next/src/pages/_app.tsx
@@ -1,8 +1,13 @@
 //header e side bar aqui
+import { ChallengesProvider } from '../contexts/ChallengesContext'
 import '../styles/global.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChallengesProvider>
+      <Component {...pageProps} />
+    </ChallengesProvider>      
+  );
 }
 
 export default MyApp
